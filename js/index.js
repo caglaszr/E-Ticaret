021b58c0427d27ba3d126e36819d4c489ae97856
Cagla Sezer <caglasezer2002@gmail.com>
2024-05-16 20:18:34 +0300

    data eklendi

--- js/index.js
+++ js/index.js
@@ -1,60 +1,6 @@
-// ! home sidebar start
-
-const btnOpenSidebar=document.querySelector("#btn-menu");
-const sidebar=document.querySelector("#sidebar");
-const btnCloseSidebar=document.querySelector("#close-sidebar");
+import headerFunc from "./header.js";
 
 
-btnOpenSidebar.addEventListener("click",function(){
-    sidebar.style.left="0";
-})
-
-btnCloseSidebar.addEventListener("click",function(){
-    sidebar.style.left="-100%";
-})
-
-/* click outside start*/ 
-
-document.addEventListener("click",function(event){
-    if(!event.composedPath().includes(sidebar) && !event.composedPath().includes(btnOpenSidebar)){
-        sidebar.style.left="-100%";
-    }
-})
-
-/* click outside end*/ 
-
-// ! home sidebar end
-
-//! search modal start
-const btnOpenSearch = document.querySelector(".search-button");
-const btnCloseSearch = document.getElementById("close-search");
-const modalSearch = document.getElementsByClassName("modal-search");
-const modalSearchWrapper = document.getElementsByClassName("modal-wrapper");
-
-btnOpenSearch.addEventListener("click", function () {
-  modalSearch[0].style.visibility = "visible";
-  modalSearch[0].style.opacity = "1";
-});
-
-btnCloseSearch.addEventListener("click", function () {
-  modalSearch[0].style.visibility = "hidden";
-  modalSearch[0].style.opacity = "0";
-});
-
-/* click outside start */
-document.addEventListener("click", function (e) {
-  if (
-    !e.composedPath().includes(modalSearchWrapper[0]) &&
-    !e.composedPath().includes(btnOpenSearch)
-  ) {
-    modalSearch[0].style.visibility = "hidden";
-    modalSearch[0].style.opacity = "0";
-  }
-});
-
-/* click outside end */
-
-//! search modal end
 
 //*? ------------
 
@@ -100,3 +46,17 @@ const dots = document.getElementsByClassName("slider-dot");
 showSlides(slideIndex);
 
 //! slider end
+
+//!  add product to localStorage
+
+async function getData() {
+    const photos = await fetch("../js/data.json");
+    const data = await photos.json();
+  
+    data ? localStorage.setItem("products", JSON.stringify(data)) : [];
+  }
+  
+  getData();
+  
+  const products = localStorage.getItem("products");
+  console.log(JSON.parse(products));
